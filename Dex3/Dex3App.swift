5106ecd151db930931b5bcf1d9d3079bc9862c80
Justin Maronde <myemailisjustin@me.com>
2024-03-21 10:21:17 -0400

    Initial Commit

---
+++ Dex3/Dex3App.swift
@@ -0,0 +1,20 @@
+//
+//  Dex3App.swift
+//  Dex3
+//
+//  Created by Justin Maronde on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Dex3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
